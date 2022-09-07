15879e39e1af702ff4378fa56e1d94ddbe628df1
Dan Matthews <dan@danmatthews.me>
2022-09-07 22:43:56 +0100

    fixed issue

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -265,7 +265,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Hey, i'm Dan.
+            {'Hey, i\'m Dan.'}
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             A web developer living in Cumbria, in the north-west of the UK. This blog is a mish-mash of things like code tutorials & tips, technology, outdoor persuits, posts about mental health, non-neurotypical issues, social issues and more.
