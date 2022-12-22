44f03576439c846ebc4d2c421efed2858d4e32e8
Dan Matthews <dan@danmatthews.me>
2022-12-22 02:48:42 +0000

    analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
@@ -22,6 +23,7 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
+    <Analytics />
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
